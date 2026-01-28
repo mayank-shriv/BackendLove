3c5dce35b6938e09968faacea25ff8f3ad6c5036
Mayank <shrivastavamayank01922@gmail.com>
2026-01-28 17:23:37 +0530

    Learning routes

--- 3Custom Api response and  Error HandlingPlusAlltheThing/src/db/index.js
+++ 3Custom Api response and  Error HandlingPlusAlltheThing/src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
   try {
-    console.log("ACTUAL MONGO URI =>", process.env.MONGODB_URI);
+    // console.log("ACTUAL MONGO URI =>", process.env.MONGODB_URI);
 
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
--- 3Custom Api response and  Error HandlingPlusAlltheThing/src/index.js
+++ 3Custom Api response and  Error HandlingPlusAlltheThing/src/index.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 const app = express();
 
-connectDB().then(()=>{
-    app.listen(process.env.PORT || 3000, ()=>{
+connectDB().then(() => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log(`Server is running at port ${process.env.PORT}`)
     })
-}).catch((err)=>{
-    console.log("MongoDB connection failed!!",err)
+}).catch((err) => {
+    console.log("MongoDB connection failed!!", err)
 });
